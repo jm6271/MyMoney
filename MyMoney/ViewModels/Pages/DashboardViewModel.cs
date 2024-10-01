01cadee73b0a632796e101a7c2ef9045878cf46c
jm6271 <107654654+jm6271@users.noreply.github.com>
2024-09-30 21:37:29 -0400

    Remove default content from the home page

--- MyMoney/ViewModels/Pages/DashboardViewModel.cs
+++ MyMoney/ViewModels/Pages/DashboardViewModel.cs
@@ -2,13 +2,6 @@
 {
     public partial class DashboardViewModel : ObservableObject
     {
-        [ObservableProperty]
-        private int _counter = 0;
 
-        [RelayCommand]
-        private void OnCounterIncrement()
-        {
-            Counter++;
-        }
     }
 }
