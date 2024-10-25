6730bc43b0b3ca476a1106d45d8bb4cabded40ec
jm6271 <107654654+jm6271@users.noreply.github.com>
2024-10-25 12:50:42 -0400

    Start on dashboard

--- MyMoney/ViewModels/Pages/DashboardViewModel.cs
+++ MyMoney/ViewModels/Pages/DashboardViewModel.cs
@@ -1,7 +1,29 @@
-﻿namespace MyMoney.ViewModels.Pages
+﻿using LiteDB;
+using MyMoney.Models;
+using System.Collections.ObjectModel;
+
+namespace MyMoney.ViewModels.Pages
 {
     public partial class DashboardViewModel : ObservableObject
     {
+        public ObservableCollection<Account> Accounts { get; set; } = [];
+
+        public DashboardViewModel() 
+        {
+            using (var db = new LiteDatabase(Helpers.DataFileLocationGetter.GetDataFilePath()))
+            {
+                // load the accounts list
+                var AccountsList = db.GetCollection<Account>("Accounts");
+
+                // iterate over the accounts in the database and add them to the Accounts collection
+                for (int i = 1; i <= AccountsList.Count(); i++)
+                {
+                    var account = AccountsList.FindById(i);
+
+                    Accounts.Add(account);
+                }
+            }
+        }
 
     }
 }
