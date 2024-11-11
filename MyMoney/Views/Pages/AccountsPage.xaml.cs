f0dbcbc8f134f07628b07bc8bc55ebd5f64cc87e
jm6271 <107654654+jm6271@users.noreply.github.com>
2024-11-11 16:56:19 -0500

    Make transactions list take up all available space

--- MyMoney/Views/Pages/AccountsPage.xaml.cs
+++ MyMoney/Views/Pages/AccountsPage.xaml.cs
@@ -21,6 +21,22 @@ namespace MyMoney.Views.Pages
         private void Page_Loaded(object sender, RoutedEventArgs e)
         {
             ViewModel.OnPageNavigatedTo();
+
+            ScrollTransactionsToBottom();
+        }
+
+        private void AddTransaction_Click(object sender, RoutedEventArgs e)
+        {
+            ViewModel.AddTransactionButtonClickCommand.Execute(null);
+
+            ScrollTransactionsToBottom();
+        }
+
+        private void ScrollTransactionsToBottom()
+        {
+            TransactionsList.SelectedIndex = TransactionsList.Items.Count - 1;
+            TransactionsList.ScrollIntoView(TransactionsList.SelectedItem);
+            TransactionsList.SelectedIndex = -1;
         }
     }
 }
