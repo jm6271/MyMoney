0358e1f19abc81afb299de90ffa8da68b931af04
jm6271 <107654654+jm6271@users.noreply.github.com>
2024-12-30 17:45:53 -0500

    Add Budget model

---
+++ MyMoney.Core/Models/Budget.cs
@@ -0,0 +1,20 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyMoney.Core.Models
+{
+    public partial class Budget : ObservableObject
+    {
+        public ObservableCollection<BudgetIncomeItem> BudgetIncomeItems { get; set; } = [];
+
+        public ObservableCollection<BudgetExpenseItem> BudgetExpenseItems { get; set; } = [];
+
+        [ObservableProperty]
+        private string _BudgetTitle = string.Empty;
+    }
+}
