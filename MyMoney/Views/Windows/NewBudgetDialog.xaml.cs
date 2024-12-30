fbafa08a8db5c1dd2985db7efa15d8c732a9483b
jm6271 <107654654+jm6271@users.noreply.github.com>
2024-12-30 12:38:47 -0500

    Add a new budget dialog

---
+++ MyMoney/Views/Windows/NewBudgetDialog.xaml.cs
@@ -0,0 +1,33 @@
+﻿using MyMoney.ViewModels.Windows;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using Wpf.Ui.Controls;
+
+namespace MyMoney.Views.Windows
+{
+    /// <summary>
+    /// Interaction logic for NewBudgetDialog.xaml
+    /// </summary>
+    public partial class NewBudgetDialog : FluentWindow
+    {
+        NewBudgetWindowViewModel ViewModel { get; set; }
+
+        public NewBudgetDialog(NewBudgetWindowViewModel viewModel)
+        {
+            ViewModel = viewModel;
+
+            InitializeComponent();
+        }
+    }
+}
