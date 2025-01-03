e69f1ed040df2dd0672f0cdd9f23ec4bf9ae59ba
jm6271 <107654654+jm6271@users.noreply.github.com>
2025-01-03 18:07:26 -0500

    Translate some models to F#

---
+++ MyMoney.Core.FS/Models/Currency.fs
@@ -0,0 +1,27 @@
+﻿namespace MyMoney.Core.FS.Models
+
+open System
+
+type Currency(value:decimal) =
+    member this.Value = value
+
+    // Overloaded ToString() method
+    // Returns a formatted string
+    override this.ToString (): string = 
+        if this.Value >= 0m then // normal number
+            $"{this.Value:N2}"
+        else // negative number, print in parenthesis
+            $"(${Math.Abs(this.Value):N2})"
+
+    // Overloaded operators
+    static member (+) (c1: Currency, c2: Currency) =
+        Currency(c1.Value + c2.Value)
+
+    static member (-) (c1: Currency, c2: Currency) =
+        Currency(c1.Value - c2.Value)
+
+    static member (*) (c1: Currency, multiplier: decimal) =
+        Currency(c1.Value * multiplier)
+
+    static member (/) (c1: Currency, divisor: decimal) =
+        Currency(c1.Value / divisor)
---
+++ MyMoney.Core.FS/Models/Transaction.fs
@@ -0,0 +1,43 @@
+﻿namespace MyMoney.Core.FS.Models
+
+open System
+
+type Transaction (date:DateTime, payee:string, category:string, spend:Currency, receive:Currency, balance:Currency, memo:string) =
+    let mutable _Payee = payee
+    let mutable _Category = category
+    let mutable _Spend = spend
+    let mutable _Receive = receive
+    let mutable _Balance = balance
+    let mutable _Memo = memo
+    let mutable _Date = date
+
+    member this.Payee 
+        with get() = _Payee
+        and set (value) = _Payee <- value
+
+    member this.Category
+        with get() = _Category
+        and set (value) = _Category <- value
+
+    member this.Spend 
+        with get() = _Spend
+        and set (value) = _Spend <- value
+
+    member this.Receive
+        with get() = _Receive
+        and set (value) = _Receive <- value
+
+    member this.Balance
+        with get() = _Balance
+        and set (value) = _Balance <- value
+
+    member this.Memo
+        with get() = _Memo
+        and set (value) = _Memo <- value
+
+    member this.Date
+        with get() = _Date
+        and set (value) = _Date <- value
+
+    member this.DateFormatted
+        with get() = _Date.ToShortDateString()
