8b83893312df2a9218e1623222d30a67ab5f6a4b
jm6271 <107654654+jm6271@users.noreply.github.com>
2025-01-03 20:25:36 -0500

    Implement rest of the models in F#

---
+++ MyMoney.Core.FS/Models/AccountDashboardDisplayItem.fs
@@ -0,0 +1,21 @@
+﻿namespace MyMoney.Core.FS.Models
+
+type AccountDashboardDisplayItem() =
+    let mutable _AccountName = ""
+    let mutable _Total = Currency(0m)
+
+    member this.AccountName 
+        with get () = _AccountName
+        and set (value) = _AccountName <- value
+
+    member this.Total
+        with get () = _Total
+        and set (value) = _Total <- value
+
+    member this.FromAccount(account: Account):unit =
+        _AccountName <- account.AccountName
+        _Total <- account.Total
+
+    member this.FromInitializers(accountName: string, total: Currency):unit =
+        _AccountName <- accountName
+        _Total <- total
---
+++ MyMoney.Core.FS/Models/BudgetItem.fs
@@ -0,0 +1,18 @@
+﻿namespace MyMoney.Core.FS.Models
+
+type BudgetItem () =
+    let mutable _Id = 0
+    let mutable _Category = ""
+    let mutable _Amount = Currency(0m)
+
+    member this.Id
+        with get () = _Id
+        and set (value) = _Id <- value
+
+    member this.Category
+        with get () = _Category
+        and set (value) = _Category <- value
+
+    member this.Amount
+        with get () = _Amount
+        and set (value) = _Amount <- value
