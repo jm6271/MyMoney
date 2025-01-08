a23fff9a108b75646f3295cd79be16bf71a9ca73
jm6271 <107654654+jm6271@users.noreply.github.com>
2025-01-08 17:59:23 -0500

    Add Accounts page tests

---
+++ MyMoney.Tests/ViewModelTests/AccountsViewModelTest.cs
@@ -0,0 +1,27 @@
+﻿using MyMoney.ViewModels.Pages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyMoney.Tests.ViewModelTests
+{
+    [TestClass]
+    public class AccountsViewModelTest
+    {
+        [TestMethod]
+        public void Test_AccountsAreLoaded()
+        {
+            AccountsViewModel viewModel = new();
+            Assert.AreEqual(1, viewModel.Accounts.Count);
+        }
+
+        [TestMethod]
+        public void Test_CategoryNamesAreLoaded()
+        {
+            AccountsViewModel viewModel = new();
+            Assert.AreEqual(7, viewModel.CategoryNames.Count);
+        }
+    }
+}
