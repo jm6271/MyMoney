968ae9f685584f74c158d626ff83a19fe8936469
jm6271 <107654654+jm6271@users.noreply.github.com>
2025-02-15 17:27:04 -0500

    Use reports page sub pages

---
+++ MyMoney/Views/Pages/ReportPages/BudgetReportsPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using MyMoney.ViewModels.Pages.ReportPages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MyMoney.Views.Pages.ReportPages
+{
+    /// <summary>
+    /// Interaction logic for BudgetReportsPage.xaml
+    /// </summary>
+    public partial class BudgetReportsPage : Page
+    {
+        public BudgetReportsViewModel ViewModel { get; private set; }
+        public BudgetReportsPage(BudgetReportsViewModel viewModel)
+        {
+            InitializeComponent();
+
+            ViewModel = viewModel;
+            DataContext = this;
+        }
+    }
+}
