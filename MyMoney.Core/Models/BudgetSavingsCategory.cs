5bf7ad723b7ebd27aa12fd51d6fee7c6a7accc1a
jm6271 <107654654+jm6271@users.noreply.github.com>
2025-06-11 16:46:39 -0400

    Implement models

---
+++ MyMoney.Core/Models/BudgetSavingsCategory.cs
@@ -0,0 +1,24 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyMoney.Core.Models
+{
+    public partial class BudgetSavingsCategory : ObservableObject
+    {
+        [ObservableProperty]
+        private int _id = 0;
+
+        [ObservableProperty]
+        private string _categoryName = string.Empty;
+
+        [ObservableProperty]
+        private Currency _currentBalance = new(0m);
+
+        [ObservableProperty]
+        private Currency _budgetedAmount = new(0m);
+    }
+}
