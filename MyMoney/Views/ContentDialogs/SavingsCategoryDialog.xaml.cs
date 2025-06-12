5fe1f84575b2cdd4e91a64131f6fd484ca291369
jm6271 <107654654+jm6271@users.noreply.github.com>
2025-06-12 11:27:27 -0400

    Make savings category dialog more user friendly

--- MyMoney/Views/ContentDialogs/SavingsCategoryDialog.xaml.cs
+++ MyMoney/Views/ContentDialogs/SavingsCategoryDialog.xaml.cs
@@ -1,18 +1,8 @@
 ﻿using MyMoney.ViewModels.ContentDialogs;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using Wpf.Ui.Controls;
 
 namespace MyMoney.Views.ContentDialogs
 {
@@ -26,6 +16,65 @@ namespace MyMoney.Views.ContentDialogs
             InitializeComponent();
             DataContext = viewModel;
             TxtCategory.Focus();
+        }
+
+        private void TxtCategory_GotKeyboardFocus(object sender, KeyboardFocusChangedEventArgs e)
+        {
+            TxtCategory.SelectAll();
+        }
+
+        private void TxtCategory_GotMouseCapture(object sender, MouseEventArgs e)
+        {
+            TxtCategory.SelectAll();
+        }
+
+        private void TxtPlanned_GotKeyboardFocus(object sender, KeyboardFocusChangedEventArgs e)
+        {
+            TxtPlanned.SelectAll();
+        }
+
+        private void TxtPlanned_GotMouseCapture(object sender, MouseEventArgs e)
+        {
+            TxtPlanned.SelectAll();
+        }
+
+        private void TxtBalance_GotKeyboardFocus(object sender, KeyboardFocusChangedEventArgs e)
+        {
+            TxtPlanned.SelectAll();
+        }
+
+        private void TxtBalance_GotMouseCapture(object sender, MouseEventArgs e)
+        {
+            TxtBalance.SelectAll();
+        }
+
+        private void ContentDialog_Closing(Wpf.Ui.Controls.ContentDialog sender, Wpf.Ui.Controls.ContentDialogClosingEventArgs args)
+        {
+            if (args.Result != ContentDialogResult.Primary) return;
+
+            // validate the user data, and if it is invalid, prevent the dialog from closing
+
+            // get validation errors for all the required fields
+            var plannedValidationErrors = Validation.GetErrors(TxtPlanned);
+            var balanceValidationErrors = Validation.GetErrors(TxtBalance);
+            var invalidCategory = string.IsNullOrEmpty(TxtCategory.Text);
+
+            // Clear the red border from custom validated controls
+            TxtCategoryBorder.BorderBrush = Brushes.Transparent;
+
+            // validate
+            if (!invalidCategory
+                              && plannedValidationErrors is not { Count: > 0 }
+                              && balanceValidationErrors is not { Count: > 0 }) return;
+            args.Cancel = true;
+
+            if (invalidCategory)
+                TxtCategoryBorder.BorderBrush = Brushes.Red;
+        }
+
+        private void TxtCategory_LostFocus(object sender, RoutedEventArgs e)
+        {
+            TxtCategoryBorder.BorderBrush = string.IsNullOrEmpty(TxtCategory.Text) ? Brushes.Red : Brushes.Transparent;
         }
     }
 }
