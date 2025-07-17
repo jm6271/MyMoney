4440954126c543b0d9593f4717c8b1f1b65c699b
jm6271 <107654654+jm6271@users.noreply.github.com>
2025-07-17 18:20:55 -0400

    Add responsive layout to DashboardPage
    Introduces a SizeChanged event handler to DashboardPage for dynamic layout adjustments based on window width. The right panel and budget report control now switch positions and margins for narrow and wide views, improving usability on different screen sizes.

--- MyMoney/Views/Pages/DashboardPage.xaml.cs
+++ MyMoney/Views/Pages/DashboardPage.xaml.cs
@@ -1,10 +1,17 @@
 ﻿using MyMoney.ViewModels.Pages;
+using System.Windows.Controls;
 using Wpf.Ui.Abstractions.Controls;
 
 namespace MyMoney.Views.Pages
 {
     public partial class DashboardPage : INavigableView<DashboardViewModel>
     {
+        private Thickness _wideReportMargin;
+        private Thickness _wideRightPanelMargin;
+
+        private Thickness _narrowReportMargin = new(0, 0, 0, 8);
+        private Thickness _narrowRightPanelMargin = new(0, 8, 0, 0);
+
         public DashboardViewModel ViewModel { get; }
 
         public DashboardPage(DashboardViewModel viewModel)
@@ -13,11 +20,40 @@ namespace MyMoney.Views.Pages
             DataContext = this;
 
             InitializeComponent();
+
+            _wideReportMargin = BudgetReport.Margin;
+            _wideRightPanelMargin = RightPanel.Margin;
         }
 
         private void Page_Loaded(object sender, RoutedEventArgs e)
         {
             ViewModel.OnPageNavigatedTo();
+        }
+
+        private void Page_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            if (e.NewSize.Width < 875)
+            {
+                // Switch to narrow layout
+                Col0.Width = new GridLength(1, GridUnitType.Star);
+                Col1.Width = new GridLength(0);
+                Grid.SetColumn(RightPanel, 0);
+                Grid.SetRow(RightPanel, 1);
+                Grid.SetRowSpan(RightPanel, 1);
+                BudgetReport.Margin = _narrowReportMargin;
+                RightPanel.Margin = _narrowRightPanelMargin;
+            }
+            else
+            {
+                // Switch to wide layout
+                Col0.Width = new GridLength(2, GridUnitType.Star);
+                Col1.Width = new GridLength(1, GridUnitType.Star);
+                Grid.SetColumn(RightPanel, 1);
+                Grid.SetRow(RightPanel, 0);
+                Grid.SetRowSpan(RightPanel, 2);
+                BudgetReport.Margin = _wideReportMargin;
+                RightPanel.Margin = _wideRightPanelMargin;
+            }
         }
     }
 }
