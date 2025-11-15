77900bbc8a734ddd7428b1cab49eee62ced3359c
jm6271 <107654654+jm6271@users.noreply.github.com>
2025-11-15 13:05:44 -0500

    Add responsive layout to dashboard

--- MyMoney/Views/Pages/DashboardPage.xaml.cs
+++ MyMoney/Views/Pages/DashboardPage.xaml.cs
@@ -11,6 +11,13 @@ namespace MyMoney.Views.Pages
 
         public DashboardViewAdapter ViewAdapter { get; }
 
+        // Cache the “wide” padding so we can restore it
+        private Thickness _wideBudgetOverviewMargin;
+        private Thickness _wideRightPanelMargin;
+
+        // Define the “narrow” padding (no left inset)
+        private readonly Thickness _narrowMargin = new Thickness(0, 0, 0, 24);
+
         public DashboardPage(DashboardViewModel viewModel)
         {
             ViewModel = viewModel;
@@ -18,6 +25,31 @@ namespace MyMoney.Views.Pages
             DataContext = this;
 
             InitializeComponent();
+
+            _wideBudgetOverviewMargin = budgetOverviewPanel.Margin;
+            _wideRightPanelMargin = RightPanel.Margin;
+        }
+
+        private void Page_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            if (e.NewSize.Width < 850)
+            {
+                // Switch to narrow layout
+                Col1.Width = new GridLength(0);
+                Grid.SetColumn(RightPanel, 0);
+                Grid.SetRow(RightPanel, 2);
+                RightPanel.Margin = _narrowMargin;
+                budgetOverviewPanel.Margin = _narrowMargin;
+            }
+            else
+            {
+                // Switch to wide layout
+                Col1.Width = new GridLength(3, GridUnitType.Star);
+                Grid.SetColumn(RightPanel, 1);
+                Grid.SetRow(RightPanel, 1);
+                RightPanel.Margin = _wideRightPanelMargin;
+                budgetOverviewPanel.Margin = _wideBudgetOverviewMargin;
+            }
         }
     }
 }
