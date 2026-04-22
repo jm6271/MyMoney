2120fd5d4ac4abf37b77e72d094cc0cb763b3cd9
jm6271 <107654654+jm6271@users.noreply.github.com>
2026-04-22 13:17:19 -0400

    Double click to edit transactions

---
+++ MyMoney/Behaviors/DoubleClickBehavior.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace MyMoney.Behaviors
+{
+    public static class DoubleClickBehavior
+    {
+        public static readonly DependencyProperty CommandProperty =
+            DependencyProperty.RegisterAttached(
+                "Command",
+                typeof(ICommand),
+                typeof(DoubleClickBehavior),
+                new PropertyMetadata(null, OnCommandChanged));
+
+        public static ICommand GetCommand(DependencyObject obj) =>
+            (ICommand)obj.GetValue(CommandProperty);
+
+        public static void SetCommand(DependencyObject obj, ICommand value) =>
+            obj.SetValue(CommandProperty, value);
+
+        private static void OnCommandChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            if (d is Control control)
+            {
+                control.MouseDoubleClick -= OnMouseDoubleClick;
+                if (e.NewValue is not null)
+                    control.MouseDoubleClick += OnMouseDoubleClick;
+            }
+        }
+
+        private static void OnMouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            var control = (Control)sender;
+            var command = GetCommand(control);
+            if (command?.CanExecute(control.DataContext) == true)
+                command.Execute(control.DataContext);
+        }
+    }
+}
