a3815fe7e0dbb4c5b70bb63801aeb8c502158ef9
jm6271 <107654654+jm6271@users.noreply.github.com>
2026-06-22 13:22:21 -0400

    Add double-click-to-edit feature to budget items

--- MyMoney/Behaviors/DoubleClickBehavior.cs
+++ MyMoney/Behaviors/DoubleClickBehavior.cs
@@ -15,11 +15,24 @@ namespace MyMoney.Behaviors
                 typeof(DoubleClickBehavior),
                 new PropertyMetadata(null, OnCommandChanged));
 
+        public static readonly DependencyProperty CommandParameterProperty =
+            DependencyProperty.RegisterAttached(
+                "CommandParameter",
+                typeof(object),
+                typeof(DoubleClickBehavior),
+                new PropertyMetadata(null));
+
         public static ICommand GetCommand(DependencyObject obj) =>
             (ICommand)obj.GetValue(CommandProperty);
 
         public static void SetCommand(DependencyObject obj, ICommand value) =>
             obj.SetValue(CommandProperty, value);
+
+        public static object GetCommandParameter(DependencyObject obj) =>
+            obj.GetValue(CommandParameterProperty);
+
+        public static void SetCommandParameter(DependencyObject obj, object value) =>
+            obj.SetValue(CommandParameterProperty, value);
 
         private static void OnCommandChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
         {
@@ -35,8 +48,10 @@ namespace MyMoney.Behaviors
         {
             var control = (Control)sender;
             var command = GetCommand(control);
-            if (command?.CanExecute(control.DataContext) == true)
-                command.Execute(control.DataContext);
+            var parameter = GetCommandParameter(control) ?? control.DataContext;
+
+            if (command?.CanExecute(parameter) == true)
+                command.Execute(parameter);
         }
     }
 }
